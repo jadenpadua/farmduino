5dd484ab025936174c2100349d79b940da4ac660
Daniel Truong <ddtruong06@gmail.com>
2019-04-15 19:52:23 -0700

    Connected front end with back end using express

--- app.js
+++ app.js
@@ -2,26 +2,49 @@
  * Resources: https://www.npmjs.com/package/node-bluetooth
  */
 
-// app.js
 
+/**** Import Modules ****/ 
 const http = require('http');
-
-// Create an instance of the http server to handle HTTP requests
-let app = http.createServer((req, res) => {  
-    // Set a response type of plain text for the response
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-
-    // Send back a response and end the connection
-    res.end('Hello World!\n');
-});
-
-// Start the server on port 3000
-app.listen(3000, '127.0.0.1');  
-console.log('Node server running on port 3000');
-
 const bluetooth = require('node-bluetooth');
+const express = require('express');
+const path = require('path');
+
+/**** Front-End Setup ****/
+const app = express();
+const router = express.Router();
+
+
+router.get('/', function(req, res){
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+})
+
+app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/images'));
+app.use('/', router);
+
+
+app.listen(process.env.port || 3000);
+console.log('Running at Port 3000');
+
+/**** Bluetooth ****/
 
 // create bluetooth device instance
 const device = new bluetooth.DeviceINQ();
+const address = ""; // represents bluetooth address of the motor to move the water pump
+const name = ""; // i think we need to find the name of the device?? I'm not 100% sure about this
 
-device.listPairedDevices(console.log);
+// // lists all the paired devices
+// device.listPairedDevices(console.log);
+
+// // connecting to Bluetooth device
+// device.findSerialPortChannel(address, function(channel){
+//   console.log('Found RFCOMM channel for serial port on %s: ', name, channel);
+ 
+//   // make bluetooth connect to remote device, we need to figure out the address for the other device
+//   bluetooth.connect(address, channel, function(err, connection){
+//     if(err) return console.error(err);
+//     connection.write(new Buffer('Hello!', 'utf-8'), () => {
+//       console.log("wrote");
+//     });
+//   });
+// });
